86523e1f1a39fe0fa1f71e4d6791cda7844cdc64
626614767@qq.com <welcome1993>
2017-08-22 20:47:51 +0800

    update item

--- article/settings.py
+++ article/settings.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 # Scrapy settings for article project
 #
 # For simplicity, this file contains only settings considered important or
@@ -65,9 +65,12 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'article.pipelines.ArticlePipeline': 300,
+   # 'article.pipelines.ArticlePipeline': 300,
    'scrapy.pipelines.images.ImagesPipeline':1
 }
+IMAGES_URLS_FIELD='font_image'
+lujing=os.path.abspath(os.path.dirname(__file__))
+IMAGES_STORE=os.path.join(lujing,"Myimages")
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
